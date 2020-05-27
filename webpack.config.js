3923a5fe51109506580ca8a8bba2bce837d1f42e
Anastasiia Tychkova <tychkovaam@gmail.com>
2020-05-26 17:24:15 -0700

    set up css minification

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,10 @@
-const MiniCssExtractPlugin = require("mini-css-extract-plugin"); // Подключили к проекту плагин
+const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackMd5Hash = require('webpack-md5-hash');
-const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin"); // Подключили к проекту плагин
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+//const webpack = require('webpack');
+const cssnano = require('cssnano');
 
 module.exports = {
     entry: {main: './src/js/index.js'},
@@ -20,14 +23,16 @@ module.exports = {
             // Добавьте ещё одно правило:
             {
 
-                test: /\.css$/, // применять это правило только к CSS-файлам
-                use: [MiniCssExtractPlugin.loader, 'css-loader'] // к этим файлам нужно применить пакеты, которые мы уже установили
-            }
+                test: /\.css$/i, // применять это правило только к CSS-файлам
+                use: [MiniCssExtractPlugin.loader, 'css-loader','postcss-loader']
+                // к этим файлам нужно применить пакеты, которые мы уже установили
+            },
+
         ]
     },
     plugins: [
         new MiniCssExtractPlugin({
-            filename: 'style.[contenthash].css'
+            filename: 'style.[contenthash].css',
         }),
         new HtmlWebpackPlugin({
             // Означает, что:
@@ -35,6 +40,18 @@ module.exports = {
             template: './src/index.html', // откуда брать образец для сравнения с текущим видом проекта
             filename: 'index.html' // имя выходного файла, то есть того, что окажется в папке dist после сборки
         }),
-        new WebpackMd5Hash()
+        new WebpackMd5Hash(),
+        new OptimizeCssAssetsPlugin({
+            assetNameRegExp: /\.css$/g,
+            cssProcessor: cssnano,
+            cssProcessorPluginOptions: {
+                preset: ['default', {
+                    discardComments: {
+                        removeAll:true
+                    },
+                    canPrint:true
+                }]
+            },
+        })
     ]
 }
